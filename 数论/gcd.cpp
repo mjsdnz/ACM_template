db61f003e8153a5f08ee50084376ea254b542496
mjsdnz <2759094765@qq.com>
2026-03-30 08:32:07 +0800

    补充gcd/exgcd

---
+++ 数论/gcd.cpp
@@ -0,0 +1,8 @@
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b);
+}
